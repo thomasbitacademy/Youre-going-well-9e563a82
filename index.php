b2680a1f88077160df41af9561c80f51718b69ab
thomasbitacademy <57895636+thomasbitacademy@users.noreply.github.com>
2020-06-24 12:04:41 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// @codingStandardsIgnoreStart
+    echo"hi";
+    $a = 2;
+// @codingStandardsIgnoreEnd
+if(2==$a){echo "ho";}
+// @codingStandardsIgnoreLine
+if($a=3){echo"beep";}
+if($a==2)
+    echo "hi";
+exit (1);
